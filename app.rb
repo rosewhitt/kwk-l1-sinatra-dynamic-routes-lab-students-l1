e43da2cc5aff86305c4b62efcd395f3ae62eb1f2
rosewhitt <rosewhitt@icloud.com>
2018-06-12 19:46:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
   get "/reversename/:name" do
     @reversename = params[:name].reverse
   end
 
+  get "/square/:number"
+    @num = params[:number]
+    @sqaure = @num*@num
 end
